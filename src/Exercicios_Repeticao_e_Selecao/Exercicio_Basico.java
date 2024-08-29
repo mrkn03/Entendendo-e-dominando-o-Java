3d4434b4b5708fa9666420e5546e15a19abbdd2a
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2024-08-29 15:14:52 -0300

    First commit

--- src/Exercicios_Repeticao_e_Selecao/Exercicio_Basico.java
+++ src/Exercicios_Repeticao_e_Selecao/Exercicio_Basico.java
@@ -4,8 +4,6 @@ package Exercicios_Repeticao_e_Selecao;
 Escreva um programa que detecte e imprima na tela números múltiplos de 3. Esses números devem variar de 1 a 100.
  */
 
-import java.util.Random;
-
 public class Exercicio_Basico {
     public static void main(String[] args) {
 
